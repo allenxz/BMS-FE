12a6237814d667b88e8a00e65154618a5d2b6b75
allenxz <13226271061@163.com>
2020-05-05 11:23:33 +0800

    refactor：修改请求头

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ import './plugins/element'
 import axios from 'axios'
 axios.defaults.baseURL = 'http://localhost:8080' 
 axios.defaults.headers.common['token'] = localStorage.getItem('token')
-axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
+axios.defaults.headers['Content-Type'] = 'application/x-www-form-urlencoded'
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
